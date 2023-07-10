baaf90a3f627bcaf7d429bbf97972712a17127c9
Kimseonbeen <ksb9242@gmail.com>
2023-07-10 22:03:21 +0900

    1. git init 2. 섹션 11 - 경로 표현식

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.jpql.*;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
@@ -17,17 +18,20 @@ public class Main {
         try {
 
             Team team = new Team();
-            team.setName("teamA");
             em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
+            Member member1 = new Member();
+            member1.setUsername("userA");
+            member1.setTeam(team);
+            em.persist(member1);
 
-            member.setTeam(team);
+            Member member2 = new Member();
+            member2.setUsername("userB");
+            member2.setTeam(team);
+            member2.setType(MemberType.ADMIN);
+            em.persist(member2);
 
-            em.persist(member);
+
 
 
             /***
@@ -64,23 +68,64 @@ public class Main {
              *                 System.out.println("member1 = " + member1);
              *             }
              *
+             *             String query = "select m.username, 'HELLO', TRUE FROM Member m " +
+             *                             "where m.type = :userType";
+             *             List<Object[]> result = em.createQuery(query)
+             *                     .setParameter("userType", MemberType.ADMIN)
+             *                     .getResultList();
+             *
+             *             for (Object[] objects : result) {
+             *                 System.out.println("objects[0] = " + objects[0]);
+             *                 System.out.println("objects[1] = " + objects[1]);
+             *                 System.out.println("objects[2] = " + objects[2]);
+             *             }
+             *
              */
 
-            String query = "select m.username, 'HELLO', TRUE FROM Member m " +
-                            "where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
+            /**
+             * 조건식 - CASE 식
+             *
+             *             String query = "select " +
+             *                             "case when m.age <= 10 then '학생요금' " +
+             *                             "     when m.age >= 60 then '경로요금' " +
+             *                             "     else '일반요금' END " +
+             *                             "from Member m";
+             *             List<String> resultList = em.createQuery(query, String.class)
+             *                     .getResultList();
+             *
+             *             for (String s : resultList) {
+             *                 System.out.println("s = " + s);
+             *             }
+             *
+             * coalesce null 치환 반환
+             *
+             *             String query = "select coalesce(m.username, '이름없는 회원') from Member m";
+             *             List<String> resultList = em.createQuery(query, String.class)
+             *                     .getResultList();
+             *
+             *             for (String s : resultList) {
+             *                 System.out.println("s = " + s);
+             *             }
+             *
+             *
+             * */
 
-            for (Object[] objects : result) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
-            }
+            /** 
+             * Member m에서 select m.team을 하게되면 묵시적 내부 조인이 일어남 
+             * 묵시적 조인은 기피해야한다. 유지보수 시 어려움
+             * */
+            String query = "select m.username From Team t join t.members m";
+            String resultList = em.createQuery(query, String.class)
+                    .getResultList().toString();
+
+            System.out.println("resultList = " + resultList);
+
+
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
